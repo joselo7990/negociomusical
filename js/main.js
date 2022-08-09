e0f4ec46b6ac9ebaa226c23f8e2a9159a1e6dd2c
unknown <josemachicote@hotmail.com>
2022-08-09 19:12:31 -0300

    data.json

--- js/main.js
+++ js/main.js
@@ -1,39 +1,54 @@
-/*const productos = [
-    { nombre: `Taylor`, precio: 550, imagen: '../imagenes/taylor1.jpg' },
-    { nombre: `Fender`, precio: 600, imagen: '../imagenes/stratocaster2.jpg' },
-    { nombre: `Gibson`, precio: 650, imagen: '../imagenes/explorer3.jpg' },
-    { nombre: `Fender jazzbass`, precio: 700, imagen: '../imagenes/jazzbass4.jpg' },
-
-];*/
-
-
-
+//
+/*fetch('./data.json')
+.then(response => response.json())
+.then(data => {
+    productos = data;
+    mostrarGuitarras()
+})
+.catch( e => console.log(e))
+*/
 
 fetch('./data.json')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        productos = data;
+        mostrarGuitarras()
+    })
     .catch(e => console.log(e))
 
-const foto$ = []
-for (let i = 0; i < productos.length; i++) {
-    const card = document.createElement('div');
-    card.classList.add('card')
-    card.classList.add('col-12', 'col-md-6', 'col-lg-3')
+let productos = [];
+const foto$ = [];
+
+function mostrarGuitarras() {
+
+    for (let i = 0; i < productos.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('card')
+        card.classList.add('col-12', 'col-md-6', 'col-lg-3')
 
 
-    card.innerHTML = `<img class="card-img-top" src="${productos[i].imagen}">
+        card.innerHTML = `<img class="card-img-top" src="${productos[i].imagen}">
                                     <div class="card-body">
                                         <h5 class="card-title">${productos[i].nombre}</h5>
                                         <p class="card-text">$ ${productos[i].precio}</p>
                                         <button onclick="agregar${i}()" class="btn btn-primary">Comprar</button>
                                       
                                     </div>`
-    document.getElementById('cont').append(card)
+        document.getElementById('cont').append(card)
+    }
 }
 
 function clickEnProducto(id) {
     alert('click en ' + id);
 }
+
+
+
+
+
+
+
+
 
 
 /*fetch('./data.json')
